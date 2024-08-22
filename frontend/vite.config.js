3dede8368c4b0e66db675e8b63d9aebe410ea6d9
Shrikant Vithoba Dalvi <157231725+shrikant-d5456@users.noreply.github.com>
2024-08-22 16:10:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    outDir: 'dist' // Vercel expects the output to be in a folder like 'dist'
+  },
 })
